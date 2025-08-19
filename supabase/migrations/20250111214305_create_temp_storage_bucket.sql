b6ed454fb1a96201d06c539ec686dca784ed780f
Rasti Aldawoodi <rastirasheed@gmail.com>
2025-08-19 19:30:41 -0400

    chore: move supabase backend to separate repo

--- supabase/migrations/20250111214305_create_temp_storage_bucket.sql
+++
@@ -1,9 +0,0 @@
-insert into storage.buckets (id, name)
-values ('temp', 'temp');
-
-create policy "Allow authenticated uploads to temp bucket"
-on storage.objects for insert
-to authenticated
-with check (
-    bucket_id = 'temp'
-);
--- supabase/migrations/20250111224230_add_select_policy_to_temp.sql
+++
@@ -1,6 +0,0 @@
-create policy "Allow authenticated select from temp bucket"
-on storage.objects for select
-to authenticated
-using (
-    bucket_id = 'temp'
-);
